c63fada27cc866223663df1d10b0f53f280db253
nogkahany <nogkahany@gmail.com>
2019-05-17 23:05:44 -0400

    app realiza el calculo aleatorio y hace el conteo

--- app/src/main/java/com/example/numeromayor/MainActivity.kt
+++ app/src/main/java/com/example/numeromayor/MainActivity.kt
@@ -4,26 +4,78 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    var valorContador:Int=0
+    var num1:Int = 0
+    var num2:Int = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        hacerloAleatorio()
     }
 
 
-    fun clickIzquierdo(view: View){
+
+
+    fun calculoIzquierdo(view: View){
+
+        if (num1>num2){
+                valorContador++
+        }
+        else{
+                valorContador--
+        }
+
+        contadorTxt.text = "Contador: $valorContador"
+
+        hacerloAleatorio()
+
+    }
+
+    fun calculoDerecho(view: View){
+
+        if (num2>num1){
+            valorContador++
+        }
+        else{
+            valorContador--
+        }
+
+        contadorTxt.text = "Contador: $valorContador"
+
+        hacerloAleatorio()
+
+    }
+
+    private fun hacerloAleatorio(){
+
+
 
         val boton_izquierdo = findViewById<Button>(R.id.btnIzquierdo)
         val boton_derecho = findViewById<Button>(R.id.btnDerecho)
 
-
         val aleatorio = Random()
 
-        val num1 = aleatorio.nextInt(10);
-        val num2 = aleatorio.nextInt(10);
+        num1 = aleatorio.nextInt(10);
+        num2 = aleatorio.nextInt(10);
+
+
+        while(num1 == num2){
+
+            num1 = aleatorio.nextInt(10);
+            num2 = aleatorio.nextInt(10);
+
+        }
 
         boton_izquierdo.text = num1.toString()
         boton_derecho.text = num2.toString()
